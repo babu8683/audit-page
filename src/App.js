b7554eba0f6a201685a5302ac34700f8cbe089e0
rajneeshrana0 <ranarajneesh038@gmail.com>
2023-08-04 03:03:09 +0530

    Audit Page Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import WasteManagementServices from './components/WasteManagementServices';
+import WasteTypeForm from './components/WasteTypeForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WasteManagementServices />
+    <WasteTypeForm />
+    </div>
+  );
+}
+
+export default App;
